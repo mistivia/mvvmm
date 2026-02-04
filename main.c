bf6eb13bbf453bf7d0fad543a99bc4fe60430138
Mistivia <i@mistivia.com>
2026-02-04 15:16:05 +0800

    reset terminal at sigint

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
 #include <termios.h>
 
@@ -9,6 +12,7 @@
 #include "serial.h"
 
 struct termios orig_termios;
+int g_term_changed = 0;
 
 void reset_terminal_mode() {
     tcsetattr(0, TCSAFLUSH, &orig_termios);
@@ -24,20 +28,47 @@ void set_terminal_raw_mode() {
     atexit(reset_terminal_mode);
     
     raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
     raw.c_cc[VMIN] = 1; // return at 1 char
     raw.c_cc[VTIME] = 0; // no timeout
     tcsetattr(0, TCSAFLUSH, &raw);
+    g_term_changed = 1;
 }
 
 void* keyboard_thread_func(void* arg) {
+    fprintf(stderr, "Press Ctrl+A & Ctrl+C to exit...\n");
     struct mvvm *vm = arg;
     set_terminal_raw_mode();
     int ch;
+    int escaped = 0;
     while ((ch = getchar()) != EOF) {
-        write_to_serial(vm, (char)ch);
+        if (escaped) {
+            escaped = 0;
+            if (ch == 0x03) {
+                kill(getpid(), SIGINT);
+            } else if (ch == 0x01) {
+                write_to_serial(vm, (char)0x01);
+            } else {
+                write_to_serial(vm, (char)0x01);
+                write_to_serial(vm, (char)ch);
+            }
+        } else {
+            if (ch == 0x01) {
+                escaped = 1;
+            } else {
+                write_to_serial(vm, (char)ch);
+            }
+        }
     }
     return NULL;
+}
+
+void sigint_handler(int sig) {
+    (void)sig;
+    if (g_term_changed) {
+        reset_terminal_mode();
+    }
+    _exit(128 + SIGINT);
 }
 
 #define MEM_SIZE (1024*1024*1024)
@@ -49,6 +80,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <kernel> <initrd>\n", argv[0]);
         return -1;
     }
+    signal(SIGINT, sigint_handler);
     if (mvvm_init(&vm, MEM_SIZE) < 0) {
         return -1;
     }
@@ -65,6 +97,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "mvvm exit code: %d\n", ret);
         return ret;
     }
+    pthread_join(keyboard_thread, NULL);
     mvvm_destroy(&vm);
     return 0;
 }
